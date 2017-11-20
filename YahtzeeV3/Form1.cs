3b7202d517dd95c1bb3f9028bbbd169b31c0bf67
Wafflebasket <Wafflebasket@outlook.com>
2017-11-20 18:39:36 +0100

    2

--- YahtzeeV3/Form1.cs
+++ YahtzeeV3/Form1.cs
@@ -10,11 +10,35 @@ using System.Windows.Forms;
 
 namespace YahtzeeV3
 {
-    public partial class Form1 : Form
+    public partial class Yahtzee : Form
     {
-        public Form1()
+        Image[] diceImages;
+        int[] dice;
+        Random rngeesus;
+        public Yahtzee()
         {
             InitializeComponent();
         }
+
+        private void Yahtzee_Load(object sender, EventArgs e)
+        {
+            diceImages = new Image[7];
+            diceImages[0] = Properties.Resources.dice1_6;
+            diceImages[1] = Properties.Resources.dice1;
+            diceImages[2] = Properties.Resources.dice2;
+            diceImages[3] = Properties.Resources.dice3;
+            diceImages[4] = Properties.Resources.dice4;
+            diceImages[5] = Properties.Resources.dice5;
+            diceImages[6] = Properties.Resources.dice6;
+            diceImages[7] = Properties.Resources.rollDice;
+             
+            dice = new int[5] { 0, 0, 0, 0, 0 };
+        }
+
+        private void rollDice_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
